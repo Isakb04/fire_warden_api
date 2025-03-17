6c32aba42618f5feaff447d69a2ee3fa0bbff2a1
Isak <isakbjarni@gmail.com>
2025-03-17 15:26:35 +0000

    changes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const dbConfig = {
     user: 'adminadmin',
     password: 'Admin12345',
     server: 'firewardentracker.database.windows.net',
-    database: 'FireWardenTracker',
+    database: 'firewardentracker',
     options: {
         encrypt: true,
         trustServerCertificate: false
